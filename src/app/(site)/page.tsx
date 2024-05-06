e934f17f396ad53b5c296ced2d7869a020b23bb3
ChinmayMohite <chinmaymohite39@gmail.com>
2024-05-06 16:01:03 +0530

    shadcn ui config and initializing landing page

---
+++ src/app/(site)/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import TitleSection from '@/components/landing-page/title-section'
+const HomePage = () => {
+  return (
+    <section>
+        <div className='overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center'>
+            
+        <TitleSection
+          pill="✨ Your Workspace, Perfected"
+          title="All-In-One Collaboration and Productivity Platform"
+        />
+        </div>
+    </section>
+  )
+}
+
+export default HomePage
